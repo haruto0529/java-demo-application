2de9553f9956499be0fd932fd04d5127e0026e17
haruto0529 <harutonan@yahoo.co.jp>
2026-05-04 01:05:41 +0900

    Create a method in the character class.

--- src/turnGame/Charactor.java
+++ src/turnGame/Charactor.java
@@ -7,14 +7,39 @@ public class Charactor {
 	protected boolean isCharging;
 
 
-	public Charactor(String name, int hp, int atk, boolean isCharging) {
+	public Charactor(String name, int hp, int atk) {
 		super();
 		this.name = name;
 		this.hp = hp;
 		this.atk = atk;
+		this.isCharging = false;
+	}
+
+	//受けたダメージ分hp減らす
+	public void takeDamage(int damage) {
+		this.hp -= damage;
+	}
+
+	//生きているかどうか
+	public boolean isArive(){
+		return this.hp > 0;
+	}
+
+	
+	
+	public boolean isCharging() {
+		return isCharging;
+	}
+	public void setCharging(boolean isCharging) {
 		this.isCharging = isCharging;
 	}
-	
-	
-
+	public String getName() {
+		return name;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public int getAtk() {
+		return atk;
+	}
 }
