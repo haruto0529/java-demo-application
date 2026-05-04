89e1aa5bf0b7bffb7723c4a7ff35cdc9ce2c193f
haruto0529 <harutonan@yahoo.co.jp>
2026-05-04 11:45:43 +0900

    battle game constractor

--- src/turnGame/BattleGame.java
+++ src/turnGame/BattleGame.java
@@ -1,5 +1,16 @@
 package turnGame;
 
 public class BattleGame {
+	private int turnCount;
+	private Player player;
+	private Enemy enemy;
+
+	public BattleGame(int turnCount) {
+		super();
+		this.turnCount = turnCount;
+		this.enemy = new Enemy();
+		this.player = new Player();
+	}
+	
 
 }
